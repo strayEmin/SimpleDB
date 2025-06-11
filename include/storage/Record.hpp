6b1369e89b32fe3ac6f173924280095e8e63b61c
StrayEmin <bungostraygit@gmail.com>
2025-06-11 19:57:48 +0300

    'Record' has been changed to meet the needs of 'Table'

--- include/storage/Record.hpp
+++ include/storage/Record.hpp
@@ -11,14 +11,20 @@ class Record {
     std::unordered_map<std::string, std::string> fields_;
 
    public:
+    Record() : fields_() {}
+
     Record(std::unordered_map<std::string, std::string> &fields)
         : fields_(fields) {}
 
     void setField(std::string field_name, std::string value);
 
-    std::string getValue(std::string field_name);
+    void deleteField(std::string field_name);
 
-    std::unordered_set<std::string> getFields();
+    void setDefault(std::string field_name, std::string default_value = "NULL");
 
-    bool matchesConditions(std::string field_name, std::string value);
+    std::string getValue(std::string field_name) const;
+
+    std::unordered_set<std::string> getFields() const;
+
+    bool matchesConditions(std::string field_name, std::string value) const;
 };
--- src/storage/Record.cpp
+++ src/storage/Record.cpp
@@ -4,15 +4,28 @@ void Record::setField(std::string field_name, std::string value) {
     fields_[field_name] = value;
 }
 
-std::string Record::getValue(std::string field_name) {
-    if (not fields_.count(field_name)) {
-        throw std::out_of_range("Field '" + field_name +
-                                "' does not exist in record");
+void Record::deleteField(std::string field_name) {
+    if (not fields_.erase(field_name)) {
+        throw std::invalid_argument("Field '" + field_name +
+                                    "' does not exist in the record");
     }
-    return fields_[field_name];
 }
 
-std::unordered_set<std::string> Record::getFields() {
+void Record::setDefault(std::string field_name, std::string default_value) {
+    fields_[field_name] = default_value;
+    // a stub that assumes further processing of the default value
+}
+
+std::string Record::getValue(std::string field_name) const {
+    try {
+        return fields_.at(field_name);
+    } catch (std::out_of_range) {
+        throw std::out_of_range("Field '" + field_name +
+                                "' does not exist in the record");
+    }
+}
+
+std::unordered_set<std::string> Record::getFields() const {
     std::unordered_set<std::string> result;
     for (auto &kv : fields_) {
         result.insert(kv.first);
@@ -21,10 +34,11 @@ std::unordered_set<std::string> Record::getFields() {
     return result;
 }
 
-bool Record::matchesConditions(std::string field_name, std::string value) {
+bool Record::matchesConditions(std::string field_name,
+                               std::string value) const {
     if (not fields_.count(field_name)) {
         throw std::out_of_range("Field '" + field_name +
                                 "' does not exist in record");
     }
-    return fields_[field_name] == value;
+    return fields_.at(field_name) == value;
 }
