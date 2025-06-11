e767b76b6273fa3b8b92a64ad7e595fb15bb27a8
StrayEmin <bungostraygit@gmail.com>
2025-06-11 20:01:06 +0300

    'Condition' has been changed after 'Record' change

--- src/utils/Condition.cpp
+++ src/utils/Condition.cpp
@@ -1,7 +1,7 @@
 #include "utils/Condition.hpp"
 
 bool Condition::evaluate(Record &record) {
-    std::string record_value = record.getField(field_name);
+    std::string record_value = record.getValue(field_name);
     // if there is no value for the key, an out_of_range exception is thrown
     return operation(record_value, value);
 }
