b562c6a78495db8752df5d78d8a314c2fcfadbd2
StrayEmin <bungostraygit@gmail.com>
2025-06-15 22:31:57 +0300

    implemented 'QueryResult'

--- include/query/QueryResult.hpp
+++ include/query/QueryResult.hpp
@@ -1,18 +1,21 @@
 #pragma once
 
+#include <list>
+#include <string>
 #include <vector>
 
-#include "../storage/Record.hpp"
-#include "string"
+#include "storage/Record.hpp"
 
 class QueryResult {
    private:
-    bool success;
-    std::vector<Record> records;
-    std::string message;
+    bool success_;
+    std::list<Record> records_;
+    std::string message_;
 
    public:
     bool isSuccess();
+
     std::string getMessage();
-    std::vector<Record> getRecords();
+
+    std::list<Record> getRecords();
 };
