313d9dd8370dfecfcb25ef981e97a2800d27c603
StrayEmin <bungostraygit@gmail.com>
2025-06-15 23:17:09 +0300

    the approach to database management has been changed. emphasis is placed on the NoSQL approach

--- include/DatabaseEngine.hpp
+++ include/DatabaseEngine.hpp
@@ -1,13 +1,17 @@
 #pragma once
 
 #include <filesystem>
+#include <list>
 #include <memory>
 #include <stdexcept>
 #include <string>
 #include <unordered_map>
+#include <utility>
+#include <vector>
 
 #include "query/Query.hpp"
 #include "query/QueryResult.hpp"
+#include "query/SelectQueryBuilder.hpp"
 #include "storage/Database.hpp"
 
 #define DB_FILE_EXTENSION ".json"
@@ -22,13 +26,38 @@ class DatabaseEngine {
    public:
     DatabaseEngine(const std::string& path_to_db_dir);
 
+    ~DatabaseEngine();
+
+    void saveChangesCurDB() const;
+
     void setDBDir(const std::string& new_db_dir);
 
-    void createDatabase(const std::string& db_name);
+    void createDatabase(const std::string& db_name, bool if_not_exist = false);
 
     void useDatabase(const std::string& db_name);
 
-    void dropDatabase(const std::string& db_name);
+    void dropDatabase(const std::string& db_name, bool if_exist = false);
 
-    QueryResult executeQuery(const Query& query);
+    SelectQueryBuilder selectFrom(
+        const std::string& table_name,
+        const std::vector<std::string>& column_names) const;
+    // to get the result, you need to use
+    // the SelectQueryBuilder class methods
+
+    void insertInto(const std::string& table_name,
+                    const std::vector<std::string>& column_names,
+                    const std::vector<std::string>& values);
+
+    std::string max(const std::string& table_name,
+                    const std::string& column_name,
+                    std::string (*max_s)(std::string, std::string));
+
+    void update(std::string table_name,
+                std::unordered_map<std::string, std::string> fieldvalues,
+                std::vector<Condition> conditions = {});
+
+    void createTable(const Table& table, bool if_not_exist = false);
+
+    void createTable(std::string table_name, std::vector<Column> columns,
+                     bool if_not_exist = false);
 };
--- src/DatabaseEngine.cpp
+++ src/DatabaseEngine.cpp
@@ -17,6 +17,13 @@ DatabaseEngine::DatabaseEngine(const std::string& path_to_db_dir) {
     }
 }
 
+void DatabaseEngine::saveChangesCurDB() const {
+    current_database_sptr_->saveToFile(
+        database_paths_.at(current_database_sptr_->getName()));
+}
+
+DatabaseEngine::~DatabaseEngine() { saveChangesCurDB(); }
+
 void DatabaseEngine::setDBDir(const std::string& new_db_dir) {
     db_dir_ = new_db_dir;
     if (*new_db_dir.rbegin() != '/') {
@@ -25,10 +32,15 @@ void DatabaseEngine::setDBDir(const std::string& new_db_dir) {
     std::filesystem::create_directory(db_dir_);
 }
 
-void DatabaseEngine::createDatabase(const std::string& db_name) {
+void DatabaseEngine::createDatabase(const std::string& db_name,
+                                    bool if_not_exist) {
     if (database_paths_.contains(db_name)) {
-        throw std::invalid_argument("this database name already exists('" +
-                                    db_name + "')");
+        if (not if_not_exist) {
+            throw std::invalid_argument("this database name already exists('" +
+                                        db_name + "')");
+        } else {
+            return;
+        }
     }
 
     Database db(db_name);
@@ -39,6 +51,9 @@ void DatabaseEngine::createDatabase(const std::string& db_name) {
 
 void DatabaseEngine::useDatabase(const std::string& db_name) {
     try {
+        if (current_database_sptr_ != nullptr) {
+            saveChangesCurDB();
+        }
         std::shared_ptr<Database> db_sptr(new Database(db_name));
         db_sptr->loadFromFile(database_paths_.at(db_name));
         current_database_sptr_ = db_sptr;
@@ -48,24 +63,88 @@ void DatabaseEngine::useDatabase(const std::string& db_name) {
     }
 }
 
-void DatabaseEngine::dropDatabase(const std::string& db_name) {
-    try {
-        if (current_database_sptr_ != nullptr and
-            current_database_sptr_->getName() == db_name) {
-            current_database_sptr_ = nullptr;
+void DatabaseEngine::dropDatabase(const std::string& db_name, bool if_exist) {
+    if (not database_paths_.contains(db_name)) {
+        if (not if_exist) {
+            throw std::out_of_range("database named '" + db_name +
+                                    "' does not exist, cannot be drop");
+        } else {
+            return;
         }
-
-        try {
-            std::filesystem::remove(database_paths_.at(db_name));
-
-        } catch (std::filesystem::filesystem_error) {
-            std::runtime_error("file by path '" + database_paths_.at(db_name) +
-                               "' can not be deleted");
-        }
-        database_paths_.erase(db_name);
-
-    } catch (std::out_of_range) {
-        throw std::out_of_range("database named '" + db_name +
-                                "' does not exist, cannot be drop");
     }
+
+    if (current_database_sptr_ != nullptr and
+        current_database_sptr_->getName() == db_name) {
+        current_database_sptr_ = nullptr;
+    }
+
+    try {
+        std::filesystem::remove(database_paths_.at(db_name));
+
+    } catch (std::filesystem::filesystem_error) {
+        std::runtime_error("file by path '" + database_paths_.at(db_name) +
+                           "' can not be deleted");
+    }
+    database_paths_.erase(db_name);
+}
+
+SelectQueryBuilder DatabaseEngine::selectFrom(
+    const std::string& table_name,
+    const std::vector<std::string>& column_names) const {
+    return SelectQueryBuilder(*current_database_sptr_->getTable(table_name),
+                              column_names);
+}
+
+void DatabaseEngine::insertInto(const std::string& table_name,
+                                const std::vector<std::string>& column_names,
+                                const std::vector<std::string>& values) {
+    if (column_names.size() != values.size()) {
+        throw std::invalid_argument(
+            "InsertInto(): there should be as many values as there are fields");
+    }
+
+    Record record;
+    for (int i = 0; i < column_names.size(); ++i) {
+        record.setField(column_names[i], values[i]);
+    }
+    current_database_sptr_->getTable(table_name)->insertRecord(record);
+    saveChangesCurDB();
+}
+
+void DatabaseEngine::createTable(const Table& table, bool if_not_exist) {
+    try {
+        current_database_sptr_->createTable(table);
+        saveChangesCurDB();
+    } catch (const std::invalid_argument& e) {
+        if (not if_not_exist) {
+            throw std::invalid_argument(e.what());
+        }
+    }
+}
+
+void DatabaseEngine::createTable(std::string table_name,
+                                 std::vector<Column> columns,
+                                 bool if_not_exist) {
+    try {
+        current_database_sptr_->createTable(table_name, columns);
+        saveChangesCurDB();
+    } catch (const std::invalid_argument& e) {
+        if (not if_not_exist) {
+            throw std::invalid_argument(e.what());
+        }
+    }
+}
+
+std::string DatabaseEngine::max(const std::string& table_name,
+                                const std::string& column_name,
+                                std::string (*max_s)(std::string,
+                                                     std::string)) {
+    const auto& table = current_database_sptr_->getTable(table_name);
+    std::string max_value;
+    std::list<std::string> values = table->getValuesInCol(column_name);
+    for (auto& v : values) {
+        max_value = max_s(max_value, v);
+    }
+
+    return max_value;
 }
