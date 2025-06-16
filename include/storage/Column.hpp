cb1911a192ca423ca2a7c27d87fa21b42fb54286
StrayEmin <bungostraygit@gmail.com>
2025-06-16 18:35:50 +0300

    the 'Columns' class has been changed, and fields have been added to implement data type checking.

--- include/storage/Column.hpp
+++ include/storage/Column.hpp
@@ -2,19 +2,28 @@
 
 #include <string>
 
+#include "utils/TypeClassifier.hpp"
+
 class Column {
    private:
+    TypeClassifier::GeneralType general_type_;
     std::string name_;
-    std::string type_;
+    std::string specific_type_;
     bool is_primary_key_;
 
    public:
     explicit Column(std::string name, std::string type, bool is_primary_key)
-        : name_(name), type_(type), is_primary_key_(is_primary_key) {}
+        : name_(name),
+          specific_type_(type),
+          is_primary_key_(is_primary_key),
+          general_type_(TypeClassifier::specificToGeneralType(type)) {}
 
     std::string getName() const;
 
-    std::string getType() const;
+    std::string getSpecificType() const;
 
     bool isPK() const;
+
+    TypeClassifier::GeneralType getGeneralType() const;
+    
 };
--- src/storage/Column.cpp
+++ src/storage/Column.cpp
@@ -2,6 +2,10 @@
 
 std::string Column::getName() const { return name_; }
 
-std::string Column::getType() const { return type_; }
+std::string Column::getSpecificType() const { return specific_type_; }
 
 bool Column::isPK() const { return is_primary_key_; }
+
+TypeClassifier::GeneralType Column::getGeneralType() const {
+    return general_type_;
+}
